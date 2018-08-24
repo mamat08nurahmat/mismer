70afa0f6c048c4ebdddd0b1465183f7a7478ea4a
mamat08nurahmat <mamat08nurahmat@gmail.com>
2018-08-24 11:18:52 +0700

    24-08-2018(v2)

---
+++ application/config/site.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| Your site name,
+|
+*/
+$config['site_name'] = 'MISMER Dashboard';
+$config['version'] = '2.6.8';
